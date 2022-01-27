56b4e5a18c11c5fcd1724c7a51e8ef91768e1a59
Leo Amaral <leo@wearenuvo.com>
2022-01-26 22:54:29 -0300

    (feature/layout): Initial Structure

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>HomePage</p>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+interface Head {
+  title: string;
+}
+
+export default Vue.extend({
+  name: "Default",
+  head(): Head {
+    return {
+      title: "Home - MktPlace",
+    };
+  },
+});
+</script>
